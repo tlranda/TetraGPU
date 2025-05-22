759a9588ffae8c2661cbc0001d15b2207952aa10
Thomas Randall <tlranda@clemson.edu>
2025-05-22 13:27:13 -0400

    Exit on failure to create dataset

--- artificial_dataset.sh
+++ artificial_dataset.sh
@@ -4,16 +4,25 @@ FORCE=1;
 
 if ! [[ -e "mwe.vtu" ]] || [[ ${FORCE} -eq 1 ]]; then
     python3 make_mesh.py --n-points 5 --output mwe;
+    if [ $? -ne 0 ]; then
+       exit;
+    fi;
 else
     echo "mwe exists";
 fi
 if ! [[ -e "tiny.vtu" ]] || [[ ${FORCE} -eq 1 ]]; then
     python3 make_mesh.py --n-points 100 --output tiny;
+    if [ $? -ne 0 ]; then
+       exit;
+    fi;
 else
     echo "tiny exists";
 fi
 if ! [[ -e "big.vtu" ]] || [[ ${FORCE} -eq 1 ]]; then
     python3 make_mesh.py --n-points 100000 --output big;
+    if [ $? -ne 0 ]; then
+       exit;
+    fi;
 else
     echo "big exists";
 fi
